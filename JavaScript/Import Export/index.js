3ed095959db38935d0f6499c096c13842f940bcf
pertinaz <manuel.agudelo1@utp.edu.co>
2024-06-21 17:00:32 -0500

    temrinandoEI

--- JavaScript/Import Export/index.js
+++ JavaScript/Import Export/index.js
@@ -1,17 +1,31 @@
 // Import and Export in ES6
 // import [defaultExport] from "module-name";
-import {
-  SECRET_NUMBER as num1,
-  add as sumar,
-  subtract,
-  divide,
-} from "./math.js";
+// import {
+//   SECRET_NUMBER as num1,
+//   add as sumar,
+//   subtract,
+//   divide,
+//   x,
+//   y,
+//   multiply,
+//   Math as MathClass  
+// } from "./math.js";
 
-function add(a, b) {
-  return a + b;
-}
-console.log(num1);
-console.log(add(1, 2));
-console.log(sumar(1, 2));
-console.log(subtract(1, 2));
-console.log(divide(1, 2));
+// function add(a, b) {
+//   return a + b;
+// }
+// console.log(num1);
+// console.log(add(1, 2));
+// console.log(sumar(1, 2));
+// console.log(subtract(1, 2));
+// console.log(divide(1, 2));
+
+// const math = new MathClass();
+
+// console.log(math.double(2));
+
+import math from './math.js';
+
+
+console.log(math.add(1, 2));
+console.loog(math.subtract(1, 2));
--- JavaScript/Import Export/math.js
+++ JavaScript/Import Export/math.js
@@ -1,20 +1,63 @@
-export const SECRET_NUMBER = 42;
+// export const SECRET_NUMBER = 42;
+// export const name = 'myMathModule';
+// export let x = 30 , y = 100;
 
-export let x,y
+// export function add(a, b) {
+//     return a + b;
+// }
 
-export function add(a, b) {
+// export function subtract(a, b) {
+//     return a - b;
+// }
+
+// export function multiply(a, b) {
+//     return a * b;
+// }   
+
+// export function divide(a, b) {
+//     return a / b;
+// }
+
+// export class Math{
+//     double (x){
+//         return x*x;
+//     }
+// }
+
+const SECRET_NUMBER = 42;
+const name = 'myMathModule';
+let x = 30 , y = 100;
+
+function add(a, b) {
     return a + b;
 }
 
-export function subtract(a, b) {
+function subtract(a, b) {
     return a - b;
 }
 
-export function multiply(a, b) {
+function multiply(a, b) {
     return a * b;
 }   
 
-export function divide(a, b) {
+function divide(a, b) {
     return a / b;
 }
 
+class Math{
+    double (x){
+        return x*x;
+    }
+}
+
+export default{
+    add,
+    subtract,
+    multiply,
+    divide,
+    SECRET_NUMBER,
+    name,
+    x,
+    y,
+    Math
+};
