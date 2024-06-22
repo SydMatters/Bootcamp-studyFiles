b9f2f2ee99561aaa2f3eba3aea012a8838a0873b
SydMatters <nicolas.vega@utp.edu.co>
2024-06-22 09:07:00 -0600

    opp

--- JavaScript/OOP/index.js
+++ JavaScript/OOP/index.js
@@ -42,13 +42,107 @@
 
 //Propoerties
 
+//Instancia de objetos
+// const user = {
+//     name: "Ryan",
+//     lastName: "Black", //propiedades
+//     age: 30,
+//     isMarried: true,
+//     hobbies: ['reading', 'running', 'coding'],
+//     address : {
+//         street : "123 Main St",
+//         city : "New York",
+//     },
+//     showFullName(){
+//         return this.name + " " + this.lastName
+//     }
+// }
+
+//Varias instancias
+// const user1 = {
+//     name: "Ryan",
+//     lastName: "Black", //propiedades
+//     age: 30,
+//     isMarried: true,
+//     hobbies: ['reading', 'running', 'coding'],
+//     address : {
+//         street : "123 Main St",
+//         city : "New York",
+//     },
+//     showFullName(){
+//         return this.name + " " + this.lastName
+//     }
+// }
+// const user2 = {
+//     name: "Ryan",
+//     lastName: "Black", //propiedades
+//     age: 30,
+//     isMarried: true,
+//     hobbies: ['reading', 'running', 'coding'],
+//     address : {
+//         street : "123 Main St",
+//         city : "New York",
+//     },
+//     showFullName(){
+//         return this.name + " " + this.lastName
+//     }
+// }
+//Metodos
+// const account = {
+//     number : "123456789",
+//     amount : 10000,
+//     deposit(saldo){
+//         this.amount += saldo;
+//         console.log(this.amount)
+//     },
+//     withDraw(gasto){
+//         if(this.amount - gasto < 0){
+//             console.log("No puede hacer el retiro")
+//         }else{
+//             this.amount -= gasto;
+//             console.log(this.amount);
+//         }
+//     }
+// }
+
+// account.deposit(100);
+// account.withDraw(200);
+// account.withDraw(9901)
+
+//Constructores
+
+// function Person (){
+//     this.name ="";
+//     this.lastName = "";
+//     this.age = 0;
+//     this.showFullName = function(){
+//         return `${this.name} ${this.lastName}`   
+//     }
+// }
+
+// const user2 = new Person();
+// user2.name = "Joe";
+// user2.lastName = "Gringo";
+// user2.age=22;
+
+// console.log(user2.showFullName());
+// console.log(user2);
+
+//Object contructor
+
+// const person =  new Object();
+// console.log(person);
+
+// const person2 = {};
+// console.log(person.constructor === Object);
+
+// const string = new Object("hello world")
+// string.name = "Special String";
+// string.test = function(){
+//     return this +"test"
+// }
+// console.log(string.name);
+
 const user = {
-    name: "Ryan", //propiedades
-    age: 30,
-    isMarried: true,
-    hobbies: ['reading', 'running', 'coding'],
-    address : {
-        street : "123 Main St",
-        city : "New York",
-    }
+    name: "",
 }
