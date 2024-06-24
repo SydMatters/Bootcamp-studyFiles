bf0703a549925df849c864ef96e8117adebba9e5
VegaSyd <nicolas.vega@utp.edu.co>
2024-06-24 15:17:53 -0500

    continueStuding

--- JavaScript/Node Practice/someInstructions.js
+++ JavaScript/Node Practice/someInstructions.js
@@ -3,3 +3,21 @@ console.log(process);
 console.log(process.env);
 // console.exit();  //Harakiri de la app
 
+//Con archivos
+
+console.log(__filename, __dirname);
+
+//Con modulos
+
+const {suma} = require('./sumar')
+
+console.log(suma(1, 2));
+
+//Qué pasa con var?
+
+//No es accesible sino es desde el nodo raiz
+var miValor = 62;
+
+console.log(miValor); //undefined
+
+console.log(global.miValor); //62
---
+++ JavaScript/Node Practice/sumar.js
@@ -0,0 +1,8 @@
+const suma = (a, b) => {
+    return a + b
+};
+
+//se puede declarar como objeto o como funcion
+module.exports = {
+    suma
+}
