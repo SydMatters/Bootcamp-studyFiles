238716d56c748153bd573180e8bf6037decedcac
VegaSyd <nicolas.vega@utp.edu.co>
2024-06-25 08:07:53 -0500

    Repaso

--- JavaScript/DOM/eventos/app.js
+++ JavaScript/DOM/eventos/app.js
@@ -21,13 +21,13 @@ const buttonHide = document.querySelector('[data-id="button-hide"]');
 
 //Manera más sensilla de hacerlo
 
-function controladorEvento(){
-    const nombre = prompt('Ingresa tu nombre');
+// function controladorEvento(){
+//     const nombre = prompt('Ingresa tu nombre');
 
-    alert(`Hola ${nombre}`);
-}
+//     alert(`Hola ${nombre}`);
+// }
 
-buttonShow.onclick = controladorEvento; //Si coloco parentesis se llama sin que pase el evento
+// buttonShow.onclick = controladorEvento; //Si coloco parentesis se llama sin que pase el evento
 
 //para eleminar controlador de evento
 // buttonShow.onclick = null;
@@ -42,11 +42,72 @@ buttonShow.onclick = controladorEvento; //Si coloco parentesis se llama sin que 
 
 //La manera más recomendada
 //Listener - Handler
-buttonShow.addEventListener('click', controladorEvento);
+// buttonShow.addEventListener('click', controladorEvento);
 //Este evento soporta todos los eventos en el dom
 
 //Puedo meter funciones
 //Se pueden acumular eventos
-buttonShow.addEventListener('click', () => {
-    alert('Hola');
-});
+// buttonShow.addEventListener('click', () => {
+//     alert('Soy otro manejador de eventos');
+// });
+
+//Es mejor escuchar los eventos desde JS
+
+function showSection(event){
+    event.preventDefault(); //Evita que se ejecute el evento por defecto
+    // console.log(event);
+    // console.log(event.target);
+    wrapper.className = "show";
+    // console.log(this); //Hace referencia al elemento que dispara el evento
+    // this.textContent = "He cambiado el texto";//This hace referencia al objeto que dispara el evento
+
+function hideSection(event){
+    // event.preventDefault();
+    // wrapper.className = "hide";
+
+}
+
+
+// const hideSection = (event) => {
+//     event.preventDefault(); //Evita que se ejecute el evento por defecto
+//     wrapper.className = "hide";
+//     //EL VALOR DE THIS ES EL OBJETO GLOBAL, cuando se usan funciones flecha
+//     console.log(this)
+// };
+
+function hideSection = (event) => {
+    event.preventDefault(); //Evita que se ejecute el evento por defecto
+    wrapper.className = "hide";
+    //EL VALOR DE THIS ES EL OBJETO GLOBAL, cuando se usan funciones flecha
+    console.log(this)
+};
+//con funciones this toma el contexto de las funciones flecha
+
+
+//Cambiando clases logro mostrar y ocultar el section
+buttonShow.addEventListener('click', showSection);
+
+buttonHide.addEventListener('click', hideSection);
+
+
+// input.addEventListener('change', (event) => {
+//     console.dir(event.target); //Muestra todas las propiedades del elemento en consola
+//     console.log(event.target.value);
+//     changeUser.textContent = event.target.value;
+// });
+
+// wrapper.addEventListener('click', (event) => {
+//     event.target.style.backgroundColor = 'hotpink';
+// });
+
+// const form = input.parentElement;
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     console.log('Formulario enviado');
+// });
+
+// form.onsubmit = function (){
+
+//     console.log('Enviado')
+// }
