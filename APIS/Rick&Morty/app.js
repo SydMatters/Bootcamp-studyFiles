5c3dd36046d814fa7bd5aa62e7efffe916f22f3b
VegaSyd <nicolas.vega@utp.edu.co>
2024-06-29 05:09:39 -0500

    buttonsAdded

--- APIS/Rick&Morty/app.js
+++ APIS/Rick&Morty/app.js
@@ -1,33 +1,69 @@
-async function getCharacters(type) {
-  try{
-    const response = await fetch(`https://rickandmortyapi.com/api/${type}`);
+var currentPage = 1;
+const maxPage = 34; 
+
+async function getCharacters(type, page) {
+  try {
+    const response = await fetch(`https://rickandmortyapi.com/api/${type}?page=${page}`);
     const data = await response.json();
-    console.log(data);
     return data;
-  } catch(error){
+  } catch (error) {
     console.log(error); 
   }
 }
 
-function createCharacters(){
-  const characters = getCharacters('character');
-  characters.then(data => {
-    const results = data.results;
-    const container = document.querySelector('.character-container');
-    results.forEach(character => {
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.innerHTML = `
-        <img src="${character.image}" alt="${character.name}" />
-        <div class="card-text-container">
-          <h2><b>Name:</b> ${character.name}</h2>
-          <h3><b>Specie:</b> ${character.species}</h3>
-        </div>
-      `;
-      container.appendChild(card);
-      console.log(results);
-    })
-    }
-)};
+function updateButtons() {
+  const prevButton = document.querySelector('.prev-button');
+  prevButton.disabled = currentPage <= 1;
+  const nextButton = document.querySelector('.next-button');
+  nextButton.disabled = currentPage >= maxPage;
+}
 
-createCharacters();
+function nextPage() {
+  const nextButton = document.querySelector('.next-button');
+  nextButton.addEventListener('click', () => {
+    if (currentPage < maxPage) {
+      currentPage++;
+      const container = document.querySelector('.character-container');
+      container.innerHTML = '';
+      createCharacters(currentPage);
+      updateButtons();
+    }
+  });
+}
+
+function previousPage() {
+  const previousButton = document.querySelector('.prev-button');
+  previousButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+      currentPage--;
+      const container = document.querySelector('.character-container');
+      container.innerHTML = '';
+      createCharacters(currentPage);
+      updateButtons();
+    }
+  });
+}
+
+async function createCharacters(page) {
+  const characters = await getCharacters('character', page);
+  const results = characters.results;
+  const container = document.querySelector('.character-container');
+  results.forEach(character => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <img src="${character.image}" alt="${character.name}" />
+      <div class="card-text-container">
+        <h2><b>Name:</b> ${character.name}</h2>
+        <h3><b>Specie:</b> ${character.species}</h3>
+      </div>
+    `;
+    container.appendChild(card);
+  });
+}
+
+createCharacters(currentPage);
+updateButtons();
+nextPage();
+previousPage();
+
