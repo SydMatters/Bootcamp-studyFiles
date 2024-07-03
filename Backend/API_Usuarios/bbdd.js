3ae81fdae64c41093fcdf4156a0074738544a5a6
SydMatters <nicolas.vega@utp.edu.co>
2024-07-03 17:32:16 -0500

    finishingAPI

---
+++ Backend/API_Usuarios/bbdd.js
@@ -0,0 +1,79 @@
+export const USERS_BBDD = [
+    [
+        {
+          "_id": "6685cf6d50f437bde2ffd64e",
+          "index": 0,
+          "guid": "50d37d39-20f2-43a9-b418-39f81f050e23",
+          "isActive": true,
+          "balance": "$1,031.07",
+          "age": 40,
+          "eyeColor": "blue",
+          "name": "Blake Valentine",
+          "gender": "male",
+          "company": "PROVIDCO",
+          "email": "blakevalentine@providco.com",
+          "phone": "+1 (984) 442-2569",
+          "address": "641 Meadow Street, Woodburn, North Dakota, 9210"
+        },
+        {
+          "_id": "6685cf6dcb7654175077fb21",
+          "index": 1,
+          "guid": "414500fd-e6fd-43ee-a9f7-6c23d2ccec44",
+          "isActive": false,
+          "balance": "$3,126.46",
+          "age": 27,
+          "eyeColor": "green",
+          "name": "Workman Lancaster",
+          "gender": "male",
+          "company": "XYLAR",
+          "email": "workmanlancaster@xylar.com",
+          "phone": "+1 (871) 516-2689",
+          "address": "833 Banner Avenue, Rote, Arizona, 9290"
+        },
+        {
+          "_id": "6685cf6d65ae0a6deacf323f",
+          "index": 2,
+          "guid": "99525f72-cdd7-4645-92ec-bbacff118fd1",
+          "isActive": false,
+          "balance": "$1,242.66",
+          "age": 21,
+          "eyeColor": "blue",
+          "name": "Denise Cannon",
+          "gender": "female",
+          "company": "TRASOLA",
+          "email": "denisecannon@trasola.com",
+          "phone": "+1 (986) 436-2841",
+          "address": "304 Eckford Street, Wiscon, California, 6871"
+        },
+        {
+          "_id": "6685cf6dc964730543e58620",
+          "index": 3,
+          "guid": "59028555-aaa7-4358-ac5e-5b6cbe9fd757",
+          "isActive": true,
+          "balance": "$1,892.43",
+          "age": 34,
+          "eyeColor": "green",
+          "name": "Baldwin Mosley",
+          "gender": "male",
+          "company": "EQUITAX",
+          "email": "baldwinmosley@equitax.com",
+          "phone": "+1 (984) 433-2574",
+          "address": "741 Hendrickson Street, Finderne, Louisiana, 3288"
+        },
+        {
+          "_id": "6685cf6d51c299075a2d5b80",
+          "index": 4,
+          "guid": "69ecdaa5-1587-48c7-a437-be0c4708a032",
+          "isActive": true,
+          "balance": "$3,494.06",
+          "age": 33,
+          "eyeColor": "brown",
+          "name": "Hatfield Rush",
+          "gender": "male",
+          "company": "OPPORTECH",
+          "email": "hatfieldrush@opportech.com",
+          "phone": "+1 (848) 404-2326",
+          "address": "927 Seagate Avenue, Salunga, North Carolina, 2902"
+        }
+      ]
+]
--- Backend/API_Usuarios/index.js
+++ Backend/API_Usuarios/index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+import { USERS_BBDD } from "./bbdd";
+//instalar dotenv
+dotenv.config();
 
 const PORT = 3000;
 const app = express();
@@ -6,8 +10,16 @@ const app = express();
 app.use(express.json());
 app.use(express.text());
 
+app.get("/account/:guid", (req, res) => {
+    console.log(req.params);
+});
 
+app.post("/account", (req, res) => {});
+
+app.patch("/account", (req, res) => {});
+
+app.delete("/account", (req, res) => {});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
