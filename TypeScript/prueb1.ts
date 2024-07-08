97b179a271e22b830984128fff11d5582225c5f2
MateoMor <mateomorales5558@gmail.com>
2024-07-08 17:26:33 -0500

    finalS

--- TypeScript/prueb1.ts
+++ TypeScript/prueb1.ts
@@ -1,26 +1,26 @@
 const persona = {
-  nombre: 'Tony',
-  apellido: 'Stark',
-  edad: 45
-}
+  nombre: "Tony",
+  apellido: "Stark",
+  edad: 45,
+};
 
-console.log(persona)
+console.log(persona);
 
-let texto = 'hola'
+let texto = "hola";
 //Cuando no sabe inferir un tipo
 
-let a //?? any cualquiera
-let anyValue: any = 'hola' //ignora el tipado del dato
+let a; //?? any cualquiera
+let anyValue: any = "hola"; //ignora el tipado del dato
 
-let unkwonValue: unknown = 'hola'//unknown no se usa mucho, quiere decir que el tipo de dato es desconocido
+let unkwonValue: unknown = "hola"; //unknown no se usa mucho, quiere decir que el tipo de dato es desconocido
 //unknown es un tipo de dato que no se puede usar en ninguna operacion
 
 //inferencia
-const e = 1
-const b = 2
-const c = e+b
+const e = 1;
+const b = 2;
+const c = e + b;
 
-let cadena = 'hola'
+let cadena = "hola";
 
 //functions
 
@@ -56,10 +56,9 @@ let cadena = 'hola'
 //   return age
 // }
 
-
 // let username : string
 
-// username = saludar({name: 'Pepe', age: 2}) //No se puede asignar un number a un string, 
+// username = saludar({name: 'Pepe', age: 2}) //No se puede asignar un number a un string,
 //Sin ponerle que devuelve la función typescript infiere que devuelve un numero gracias a age
 
 //forma correcta
@@ -107,7 +106,6 @@ let cadena = 'hola'
 //   throw new Error(message)
 // }
 
-
 // //Las funciones no tienen inferencia de tipos, hay que ponerlos siempre
 
 // const avengers = ['Ironman', 'Thor', 'Capitan America']
@@ -117,7 +115,6 @@ let cadena = 'hola'
 // avengers.forEach(function (avenger){
 //   console.log(avenger.toUpperCase())
 // })
-
 
 // //Objetos. tiene inferencia de datos
 
@@ -164,7 +161,7 @@ let cadena = 'hola'
 //   const {name, age} = heroe
 //   return {name, age}
 // }
-// //Ahora espera un objeto de tipo Hero 
+// //Ahora espera un objeto de tipo Hero
 // const thor3 = createHero3({name: 'Thor', age: 1500})
 
 //hay una forma de hacerlo más corto
@@ -208,7 +205,7 @@ let cadena = 'hola'
 //   name: string,
 //   age: number,
 //   isActive?: boolean
-// } 
+// }
 
 // function create(hero: Hero): Hero {
 //   const {name, age} = hero
@@ -233,15 +230,13 @@ let cadena = 'hola'
 // let ann : number | string
 // ann = 2 //Valido
 
-
-
 // type Hero = {
 //   readonly id?: HeroID,
 //   name: string,
 //   age: number,
 //   isActive?: boolean,
 //   powerScale?: HeroPowerScale
-// } 
+// }
 
 // function create(hero: Hero): Hero {
 //   const {name, age} = hero
@@ -259,34 +254,159 @@ let cadena = 'hola'
 
 //Intersectuon types
 
-type HeroID = `${string}-${string}-${string}-${string}-${string}`
-type HeroPowerScale = 'low' | 'medium' | 'high' | 'super' | 'godlike' //La escala de poderes de los heroes
-//puede ser low, medium, high, super, godlike
+// type HeroID = `${string}-${string}-${string}-${string}-${string}`
+// type HeroPowerScale = 'low' | 'medium' | 'high' | 'super' | 'godlike' //La escala de poderes de los heroes
+// //puede ser low, medium, high, super, godlike
 
-let ann : number | string
-ann = 2 //Valido
+// let ann : number | string
+// ann = 2 //Valido
 
+// type HeroBasicInfo = {
+//   name: string,
+//   age: number
 
+// }
+// type HeroProperties = {
+//   readonly id?: HeroID,
+//   isActive?: boolean,
+//   powerScale?: HeroPowerScale
+// }
 
-type Hero = {
-  readonly id?: HeroID,
-  name: string,
-  age: number,
-  isActive?: boolean,
-  powerScale?: HeroPowerScale
-} 
+// type Hero = HeroBasicInfo & HeroProperties //Intersección de tipos parecido a un AND nuevo tipo con dos propiedades
 
-function create(hero: Hero): Hero {
-  const {name, age} = hero
-  return {
-    id: crypto.randomUUID(),
-    name,
-    age,
-    isActive: true
-  }
+// function create(hero: Hero): Hero {
+//   const {name, age} = hero
+//   return {
+//     id: crypto.randomUUID(),
+//     name,
+//     age,
+//     isActive: true
+//   }
+// }
+
+// const thor = create({name: 'Thor', age: 1500})
+
+// thor.powerScale = 'high'
+
+//Type INdex
+
+// type HeroProperties = {
+//   isActive?: boolean,
+//   address:{
+//     city: string,
+//     country: string
+//   }
+// }
+
+// const addressHero : HeroProperties['address'] = {
+//   city: 'New York',
+//   country: 'USA'
+// }
+
+// const address = {
+//   city: 'New York',
+//   country: 'USA'
+// }
+
+// type Address = typeof address
+
+// const addressHero : Address = {
+//   city: 'New York',
+//   country: 'USA'
+// }
+
+//Type from fuction return
+// function createAddress () {
+//   return {
+//     city: 'New York',
+//     country: 'USA'
+//   }
+// }
+
+// type Address = ReturnType<typeof createAddress>
+
+//Arrays
+
+// const languages = [] //lo que le decimos a typescrit es que es un array never
+// const languages2 : string []= [] //Array de strings
+// languages2.push('JavaScript')
+// langueages2.push(2) //No se puede añadir un number a un array de strings
+
+// const languages3 : Array<string> = [] //Array de strings
+
+// const languages4 : (string | number) [] = [] //Array de strings o numeros
+
+// languages4.push('JavaScript')
+// languages4.push(2)
+
+//Matrices
+//Tuplas
+// type CellValue = "X" | "O" | null;
+// type GameBoard = [
+//   [CellValue, CellValue, CellValue],
+//   [CellValue, CellValue, CellValue],
+//   [CellValue, CellValue, CellValue]
+// ];
+
+// const board: GameBoard = [
+//   ["X", "O", null],
+//   ["X", "O", null],
+//   ["X", "O", null]
+// ];
+
+// type State = [string, (newName:string) => void]
+
+// const [hero,setHero]:State = useState('thor')
+
+// type RGB = [number, number, number]; //0 - 255
+
+// const RGB = [255, 0, 0]; //0 - 255
+
+//Enums
+//Para colecciones de datos finitos
+
+//utilizar const enum para no generar codigo de más
+// //utilizar enum para generar un componenete a consumir fuera de la app
+// const enum ERROR_TYPES{
+//   NOT_FOUND,
+//   UNAUTHORIZED,
+//   FORBIDDEN
+// }
+
+// function mostrarMensaje (tipoDeError : ERROR_TYPES){
+//   if(tipoDeError === ERROR_TYPES.NOT_FOUND){
+//     console.log('No se ha encontrado el recurso')
+//   }
+//   else if(tipoDeError === ERROR_TYPES.UNAUTHORIZED){
+//     console.log('No estás autorizado')
+//   }
+//   else if(tipoDeError === ERROR_TYPES.FORBIDDEN){
+//     console.log('No tienes permisos')
+//   }
+// }
+
+//El document puede devolve
+// null si no lo encuentra
+//HTMLElement si lo encuentra
+//HTMLCanvasElement si lo encuentra
+
+// ??? como sabe TypeScript que realmente estas recuperando un elemento <canvas> y no un <div> o cualquier otro elemento?
+
+//con esta implementacion nos estamos perdiendo de la comprobación por NULL, ya que al ser HTMLCanvasElement no puede ser null 
+const canvas = document.getElementById('canvas') as HTMLCanvasElement //Type assertion
+if (canvas != null) {
+  const ctx = canvas //Si canvas existe, haz esto
 }
 
-const thor = create({name: 'Thor', age: 1500})
+//Verificando que sea un canvas
+const canvas1 = document.getElementById('canvas')
+if (canvas1 != null){
+  const ctx = (canvas1 as HTMLCanvasElement).getContext('2d') //Type assertion
+}
 
-thor.powerScale = 'high'  
+//inferencia = typescript se da cuenta dentro del if ua solo el canvas va a poder ser un HTMLCanvasElement 
+const canvas2 = document.getElementById('canvas')
+if (canvas2 != null && canvas2 instanceof HTMLCanvasElement){
+  const ctx = canvas2.getContext('2d') //Type assertion
+}
 
